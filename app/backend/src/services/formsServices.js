98d79bdc637f05cd8c8338fa3f2ad12bdafe47bf
Williamns <williamnstolb@gmail.com>
2022-04-30 23:07:54 -0300

    createForms do services criado sem verificação

---
+++ app/backend/src/services/formsServices.js
@@ -0,0 +1,12 @@
+const { createFormsModel } = require('../models/formsModel');
+
+const createFormsService = async (forms) => {
+  // const quantValidate = calcQuantity(forms);
+  // if(!quantValidate) return { errCode: 400, message: 'Valores inválidos' };
+
+  // const { error } = dataValidation.validate({ ...forms });
+  // if (error) return { errCode: 400, message: error.message };
+
+  const updatedFile = await createFormsModel(forms);  
+  return updatedFile;
+};
