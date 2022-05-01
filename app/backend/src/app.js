344facd8232d33465b032762cb8b338fcea56c56
Williamns <williamnstolb@gmail.com>
2022-05-01 01:58:21 -0300

    fix: app corrigido

--- app/backend/src/app.js
+++ app/backend/src/app.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { createForms, getForms } = require('./controllers/formsController');
-<<<<<<< HEAD
 const Cors = require('cors');
 
 const app = express();
   app.use(bodyParser.json());
   app.use(Cors());
-=======
-const cors = require('cors');
-
-const app = express();
-  app.use(bodyParser.json());
-  app.use(cors());
->>>>>>> 4ea2d020e86ab88076031b8b5a12b37f801d3201
 
   app.post('/forms', createForms);
   app.get('/forms', getForms);
