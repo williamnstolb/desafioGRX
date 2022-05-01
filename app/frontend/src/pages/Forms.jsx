4d88f09d67b26c2df793fd4699259aa019cd44fa
Williamns <williamnstolb@gmail.com>
2022-04-30 21:04:10 -0300

    Estrutura básica do Forms

--- app/frontend/src/pages/Forms.jsx
+++ app/frontend/src/pages/Forms.jsx
@@ -3,14 +3,40 @@ import React, { Component } from 'react'
 export default class Forms extends Component {
   constructor() {
     super()
+
     this.state = {
-    
+      Pergunta1: '',
+      Pergunta2: '',
+      Pergunta3: '',
+      Pergunta4: '',
     }
   }
 
   render() {
     return (
-      <div><h2>Forms</h2></div>
+      <div>
+        <form >
+        <label htmlFor='pergunta1'>
+           <h3>1) Você se considera bom em lógica?</h3>       
+           <p >botao</p>
+        </label>
+        <label htmlFor='pergunta2'>
+            <h3>2) Gosta de aprender com desafios?</h3>
+            <p >botao</p>
+          </label>                    
+          <label htmlFor='pergunta3'>
+            <h3>3) Gostaria de fazer parte da GRX?</h3>
+            <p >select</p>
+          </label>
+          <label htmlFor='pergunta4'>
+            <h3>4) Por favor, justifique a resposta anterior.</h3>
+            <p >select</p>
+          </label>
+          <div>
+            <input type="submit" />
+          </div>
+        </form>
+      </div>
     )
   }
 }
